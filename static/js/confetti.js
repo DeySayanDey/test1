c26972fa307a99fd6a999728d32fbb635d39443d
Sayan Dey <118963559+DeySayanDey@users.noreply.github.com>
2025-08-31 01:44:15 +0530

    Delete static directory

--- static/js/confetti.js
+++
@@ -1,4 +0,0 @@
-function startConfetti() {
-    // Implement confetti animation logic here
-    console.log("Confetti animation started!");
-}
--- static/js/script.js
+++
@@ -1,7 +0,0 @@
-if (document.documentElement.requestFullscreen) {
-        document.documentElement.requestFullscreen();
-      } else if (document.documentElement.webkitRequestFullscreen) {
-        document.documentElement.webkitRequestFullscreen(); // Safari
-      } else if (document.documentElement.msRequestFullscreen) {
-        document.documentElement.msRequestFullscreen(); // IE/Edge
-      }
