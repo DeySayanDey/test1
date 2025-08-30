f0723189e815789139f450745609f848970a9564
Sayan Dey <118963559+DeySayanDey@users.noreply.github.com>
2025-08-31 01:29:00 +0530

    Delete birthday-surprise-website directory

--- birthday-surprise-website/static/js/music.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const music = document.getElementById("bg-music");
-  music.volume = 0.5;
-
-  // For autoplay to work reliably, trigger play on first interaction
-  document.body.addEventListener("click", () => {
-    music.play().catch(() => {});
-  }, { once: true });
-});
